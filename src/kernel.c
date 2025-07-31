d6e341b4344bceddd852753128adb9be5e502474
flixytss <flixytssyutu@gmail.com>
2025-07-31 18:11:03 -0400

    kernel

--- src/kernel.c
+++ src/kernel.c
@@ -1,8 +1,20 @@
 // src/kernel.c
-void _start() {
+void print(int y, char* string){
     char* video = (char*) 0xB8000;
-    video[0] = 'K';
-    video[1] = 0x07;
+    int x = 0;
+    while (string[x] != '\0') {
+        int offset = (y * 80 + x) * 2;
+        video[offset] = string[x];
+        video[offset + 1] = 0x07;
+        x++;
+    }
+}
+
+void _start() {
+    char* text = "Hola";
+    //print(10, &text);
+
+    //print(10, text);
 
     while (1);
 }
