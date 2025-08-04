f2e3e2b3bba996d3f03bc74b3c7bbcdf36325135
flixytss <flixytssyutu@gmail.com>
2025-08-04 17:25:16 -0400

    kerne

--- src/kernel.c
+++ src/kernel.c
@@ -14,6 +14,18 @@ void print(unsigned int x, unsigned int y, const char* c){
     }
 }
 
+//Cursor
+void outb(unsigned short port, unsigned char value){__asm__ volatile ("outb %0, %1" : : "a"(value), "Nd"(port));}
+void move_cursor(unsigned int x, unsigned int y){
+    unsigned short pos = y*80+x;
+
+    outb(0x3D4, 14);
+    outb(0x3D5, (unsigned char)pos>>8);
+
+    outb(0x3D4, 15);
+    outb(0x3D5, (unsigned char)pos&0xFF);
+}
+
 //Input
 const char scancode_ascii[] = {
     0, 27, '1', '2', '3', '4', '5', '6', '7', '8',
@@ -30,14 +42,22 @@ static inline unsigned char inb(){
     return result;
 }
 char input(){
-    char s=0;
-    do{s=inb();}while(s==0);
-
-    printchar(0, 0, ScanCode_To_Ascii(s));
+    unsigned char scancode = 0;
+    char ascii = 0;
+    do {
+        scancode = inb();
+        if (scancode & 0x80) continue;
+        if (scancode < sizeof(scancode_ascii)) {ascii = scancode_ascii[scancode];}
+    } while (ascii == 0);
+    return ascii;
 }
 
+
 __attribute__((section(".text.start"))) void _start(){
-    input();
+    move_cursor(0, 24);
+
+    char s = input();
+    printchar(0, 0, s);
 
     while (1);
 }
