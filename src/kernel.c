b1967dbda32821e7360039d692609b1ce920489f
flixytss <flixytssyutu@gmail.com>
2025-08-04 19:19:07 -0400

    kernel

--- src/kernel.c
+++ src/kernel.c
@@ -1,3 +1,5 @@
+#define MEMORY_SIZE (1024*8)
+
 void printchar(unsigned int x, unsigned int y, char c){
     volatile char* video = (volatile char*)0xB8000;
     int offset = (y*80+x)*2;
@@ -13,6 +15,7 @@ void print(unsigned int x, unsigned int y, const char* c){
         printchar(x+i, y, c[i]);
     }
 }
+//Memory
 
 //Cursor
 void outb(unsigned short port, unsigned char value){__asm__ volatile ("outb %0, %1" : : "a"(value), "Nd"(port));}
@@ -20,21 +23,22 @@ void move_cursor(unsigned int x, unsigned int y){
     unsigned short pos = y*80+x;
 
     outb(0x3D4, 14);
-    outb(0x3D5, (unsigned char)pos>>8);
+    outb(0x3D5, (unsigned char)(pos>>8));
 
     outb(0x3D4, 15);
-    outb(0x3D5, (unsigned char)pos&0xFF);
+    outb(0x3D5, (unsigned char)(pos&0xFF));
 }
 
 //Input
 const char scancode_ascii[] = {
-    0, 27, '1', '2', '3', '4', '5', '6', '7', '8',
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',
     '9', '0', '-', '=', '\b', '\t', 'q', 'w', 'e', 'r',
-    't', 'y', 'z', 'u', 'i', 'o', 'p', '[', ']', '\n',
-    0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
-    ';', '\'', '`', 0, '\\', 'z', 'x', 'c', 'v', 'b',
-    'n', 'm', ',', '.', '/', 0, '*', 0, ' ', 0
+    't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n', 0,
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
+    '\'', '`', 0, '\\', 'z', 'x', 'c', 'v', 'b', 'n',
+    'm', ',', '.', '/', 0, '*', 0, ' ', 0, 0
 };
+
 static inline char ScanCode_To_Ascii(unsigned char inb){return scancode_ascii[inb];}
 static inline unsigned char inb(){
     char result;
@@ -44,21 +48,39 @@ static inline unsigned char inb(){
 char input(){
     unsigned char scancode = 0;
     char ascii = 0;
-    do {
+    while (1) {
         scancode = inb();
         if (scancode & 0x80) continue;
-        if (scancode < sizeof(scancode_ascii)) {ascii = scancode_ascii[scancode];}
-    } while (ascii == 0);
-    return ascii;
+        if (scancode < sizeof(scancode_ascii)) {
+            ascii = scancode_ascii[scancode];
+            if (ascii != 0) {
+                while ((inb() & 0x80) == 0);
+                if(scancode==0x1C)return 0x1C;
+                if(scancode==0x0E)return 0x0E;
+                else return ascii;
+            }
+        }
+    }
 }
 
-
 __attribute__((section(".text.start"))) void _start(){
-    move_cursor(0, 24);
+    int x=0;
+    char s;
+    while(1){
+        s = input();
 
-    char s = input();
-    printchar(0, 0, s);
+        if((unsigned char)s==0x0E){
+            printchar(x-1, 0, ' ');
+            move_cursor(x-1, 0);
+            x--;
+        }
+        else{
+            printchar(x, 0, s);
+            move_cursor(x+1, 0);
+            x++;
+        }
+    }
 
     while (1);
 }
-//#include <>
+//#include <> 0x1C
